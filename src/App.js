a65e15d4967bea0b396d70d9ea27f9cb7d1aa832
ptejas2 <tejas.patel@amns.in>
2022-12-23 02:15:38 +0530

    integrated google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactGA from 'react-ga';
+import RouteChangeTracker from './RouteChangeTracker'
+
+
+
+const TRACKING_ID = "UA-252607377-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
     <div className="App">
+      
+      <RouteChangeTracker/>
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>Hello from Tejas !</h1>

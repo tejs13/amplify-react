172bb6fa6c452ac589330ada77e2eedb3c4a0275
PATEL <tejas.p@csu.fullerton.edu>
2023-02-07 01:35:21 -0800

    added homescreen

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ReactGA from 'react-ga';
 import RouteChangeTracker from './RouteChangeTracker'
-
+import HomeScreen from './HomeScreen/Screens/HomeScreen';
 
 
 const TRACKING_ID = "UA-252607377-1"; // YOUR_OWN_TRACKING_ID
@@ -11,13 +11,10 @@ ReactGA.initialize(TRACKING_ID);
 function App() {
   return (
     <div className="App">
-      
+      <HomeScreen/>
       <RouteChangeTracker/>
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>Hello from Tejaskumar !</h1>
-      </header>
-    </div>
+      </div>
+    
   );
 }
 
---
+++ src/HomeScreen/Screens/HomeScreen.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import { Button } from 'antd';
+
+
+class HomeScreen extends React.Component{
+
+    render() {
+        return (
+            <>
+            <header className="App-header">
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                <h1>Hello from Tejaskumar !!!</h1>
+                <Button type="primary">Start Here</Button>
+            </header>
+            </>
+        )
+    }
+
+
+}
+
+
+
+export default HomeScreen
+
+
+
